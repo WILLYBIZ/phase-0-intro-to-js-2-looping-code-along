469921627b95c01de93c381df12e31b633b86a6a
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-08-21 14:10:44 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solutions in this file
+
+// function writeCards(cards,event) {
+    
+
+//     let  newCard = [];
+//         for (let i = 0; i < cards.length; i++) {
+//       newCard.push(`Thank you, ${cards[i]}, for the wonderful ${event} surprise  gift!`);
+//         }
+        
+//         return newCard;
+
+//         }
+    
+//     function countDown(int){
+//         while(int>=0){
+//             console.log(int);
+//             int--;
+//         }
+//     }
+
+ 
+function writeCards(names, event) {
+    let newCard = [];
+    for(let i = 0; i < names.length; i++) {
+        newCard.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newCard;
+}
+
+function countDown(int) {
+    while(int  >= 0) {
+        console.log(int);
+        int --;
+    }
+}
+
+
